d61a0209697e19fc67f9ce0476923e357daf88b0
samanthamarienock <sammarienock@gmail.com>
2019-02-19 14:40:29 -0800

    excercise 7

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,7 @@
 class Employee < ActiveRecord::Base
-
+belongs_to :store
+validates :first_name { minimum: 1 }
+validates :last_name { minimum: 1 }
+validates :hourly_rate, numericality: { only_integer: true, greater_than_or_equal_to: 40 }
+validates_presence_of :store_id
 end
